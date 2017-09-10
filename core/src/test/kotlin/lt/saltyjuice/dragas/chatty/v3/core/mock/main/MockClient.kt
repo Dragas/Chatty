87c679855f7500df5c0828d130640f6f2b3a55f9
Dragas <mgridinas@gmail.com>
2017-09-10 12:09:23 +0300

    fixes an issue where routes would be cloned incorrectly

--- core/src/test/kotlin/lt/saltyjuice/dragas/chatty/v3/core/mock/main/MockClient.kt
+++ core/src/test/kotlin/lt/saltyjuice/dragas/chatty/v3/core/mock/main/MockClient.kt
@@ -1,7 +1,8 @@
-package lt.saltyjuice.dragas.chatty.v3.core.mock
+package lt.saltyjuice.dragas.chatty.v3.core.mock.main
 
 import lt.saltyjuice.dragas.chatty.v3.core.controller.Controller
 import lt.saltyjuice.dragas.chatty.v3.core.main.Client
+import lt.saltyjuice.dragas.chatty.v3.core.mock.route.MockRouter
 
 class MockClient(vararg controllers: Class<out Controller>) : Client(*controllers)
 {
