06e06eee98ee5ef52650f1e523387381028f063c
Dragas <mgridinas@gmail.com>
2017-09-19 09:39:37 +0300

    events are now interfaces

--- core/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/core/event/DisconnectEvent.kt
+++ core/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/core/event/DisconnectEvent.kt
@@ -1,5 +1,5 @@
 package lt.saltyjuice.dragas.chatty.v3.core.event
 
-class DisconnectEvent @JvmOverloads constructor(override val payload: Int = 1) : Event(payload)
+class DisconnectEvent @JvmOverloads constructor(override val payload: Int = 1) : Event
 {
 }
--- core/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/core/event/Event.kt
+++ core/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/core/event/Event.kt
@@ -4,7 +4,7 @@ package lt.saltyjuice.dragas.chatty.v3.core.event
  * Base class for all event types. Implementations need to override payload type to match
  * the target listener's parameter so that framework could properly handle it.
  */
-open class Event constructor(open val payload: Any)
+interface Event
 {
-
+    val payload: Any
 }
