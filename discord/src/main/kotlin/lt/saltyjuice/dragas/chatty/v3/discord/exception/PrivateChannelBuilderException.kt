2824bf74afbece8b5b131081ca6c0f00997bb895
Dragas <mgridinas@gmail.com>
2017-09-28 21:34:12 +0300

    Throws exceptions

---
+++ discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/exception/PrivateChannelBuilderException.kt
@@ -0,0 +1,5 @@
+package lt.saltyjuice.dragas.chatty.v3.discord.exception
+
+open class PrivateChannelBuilderException @JvmOverloads constructor(message: String = "") : BuilderException(message)
+{
+}
