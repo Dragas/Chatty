ae014b95a43220cec999625e3e94d4e5cd6c7b8c
Dragas <mgridinas@gmail.com>
2017-09-28 21:19:50 +0300

    refactored channel builder

---
+++ discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/message/builder/PrivateChannelBuilder.kt
@@ -0,0 +1,47 @@
+package lt.saltyjuice.dragas.chatty.v3.discord.message.builder
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import lt.saltyjuice.dragas.chatty.v3.discord.api.Utility
+import lt.saltyjuice.dragas.chatty.v3.discord.message.general.Channel
+import lt.saltyjuice.dragas.chatty.v3.discord.message.general.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+
+/**
+ * Used to create a direct channel with particular user
+ */
+open class PrivateChannelBuilder(id: String) : Builder<Channel>
+{
+    @Expose
+    @SerializedName("recipient_id")
+    var usarId: String = id
+
+    /**
+     * a shorthand for calling `PrivateChannelBuilder(user.id)`
+     */
+    constructor(user: User) : this(user.id)
+
+    @Throws(IOException::class, RuntimeException::class)
+    override fun send(): Response<Channel>
+    {
+        return Utility.discordAPI.createChannel(this).execute()
+    }
+
+    override fun sendAsync(callback: Callback<Channel>)
+    {
+        Utility.discordAPI.createChannel(this).enqueue(callback)
+    }
+
+    override fun onFailure(call: Call<Channel>, t: Throwable)
+    {
+        t.printStackTrace(System.err)
+    }
+
+    override fun onResponse(call: Call<Channel>, response: Response<Channel>)
+    {
+
+    }
+}
