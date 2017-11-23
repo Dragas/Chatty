8bc58e1d2a7bd959cd58efac66880c0f5ec6ee34
Mantas Gridinas <mgridinas@gmail.com>
2017-11-23 15:05:12 +0200

    separates logic

--- discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/api/interceptor/GlobalRateLimiter.kt
+++ discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/api/interceptor/GlobalRateLimiter.kt
@@ -1,16 +1,9 @@
 package lt.saltyjuice.dragas.chatty.v3.discord.api.interceptor
 
 import okhttp3.Request
-import okhttp3.Response
-import java.util.*
-import java.util.concurrent.atomic.AtomicInteger
-import java.util.concurrent.atomic.AtomicLong
 
-open class GlobalRateLimiter : AbstractRateLimitInterceptor()
+open class GlobalRateLimiter : AccountLimiter()
 {
-    protected val lastRequest = AtomicLong(0)
-    protected val requestCount = AtomicInteger(0)
-
     override fun canLimit(request: Request): Boolean
     {
         return true
@@ -21,58 +14,11 @@ open class GlobalRateLimiter : AbstractRateLimitInterceptor()
         return RESET_DELAY
     }
 
-    override fun waitForLimit(limit: Limit)
-    {
-        val delay = getTimeSinceLastRequest(limit)
-        if (delay <= getResetDelay() && delay > 0)
-            Thread.sleep(delay)
-    }
-
-    override fun storeLimit(response: Response)
-    {
-        val now = Date().time
-        val lastRequestTime = lastRequest.get()
-        val delay = now - lastRequestTime
-        if (delay > getResetDelay())
-        {
-            lastRequest.set(now)
-            requestCount.set(0)
-        }
-        requestCount.incrementAndGet()
-    }
-
-    override fun getCurrentRequestCount(limit: Limit): Int
-    {
-        return getCurrentRequestCount()
-    }
-
-    open fun getCurrentRequestCount(): Int
-    {
-        return requestCount.get()
-    }
-
-    override fun identify(request: Request): Limit?
-    {
-        return Limit()
-    }
-
     override fun getMaxRequests(): Int
     {
         return MAX_REQUESTS
     }
 
-    override fun getTimeSinceLastRequest(limit: Limit): Long
-    {
-        return getTimeSinceLastRequest()
-    }
-
-    open fun getTimeSinceLastRequest(): Long
-    {
-        val now = Date().time
-        val lastRequestTime = lastRequest.get()
-        val delay = now - lastRequestTime
-        return delay
-    }
 
     companion object
     {
