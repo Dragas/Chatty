e36e3e998dcb56ff646243a266e2c0e6b8569175
Mantas Gridinas <mgridinas@gmail.com>
2017-11-23 14:51:03 +0200

    Adds reaction limiter
    Reaction limiter, like message posting and deleting limiters, doesn't really have any
    unique logic, since it's limited to channel basis, thus only contains different constants.

---
+++ discord/src/main/kotlin/lt/saltyjuice/dragas/chatty/v3/discord/api/interceptor/ReactionLimiter.kt
@@ -0,0 +1,40 @@
+package lt.saltyjuice.dragas.chatty.v3.discord.api.interceptor
+
+import okhttp3.Request
+
+open class ReactionLimiter : ChannelLimiter()
+{
+    override fun getRequiredMethod(): Array<String>
+    {
+        return arrayOf("PUT", "DELETE")
+    }
+
+    override fun getSegmentSize(): Int
+    {
+        return 7
+    }
+
+    override fun getMaxRequests(): Int
+    {
+        return MAX_REQUESTS
+    }
+
+    override fun getResetDelay(): Long
+    {
+        return RESET_DELAY
+    }
+
+    override fun canLimit(request: Request): Boolean
+    {
+        return super.canLimit(request) && request.url().toString().contains("reaction", true)
+    }
+
+    companion object
+    {
+        @JvmStatic
+        val MAX_REQUESTS = 1
+
+        @JvmStatic
+        val RESET_DELAY = 250L
+    }
+}
